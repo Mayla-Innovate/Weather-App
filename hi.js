c266923a45e317d5a8ec230a281d2778b95e01f1
Mayla-Innovate <83228686+Mayla-Innovate@users.noreply.github.com>
2021-06-26 19:29:14 +0100

    Updated the days on JS

--- hi.js
+++ hi.js
@@ -7,7 +7,15 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   let day = days[date.getDay()];
   return `${day}, ${hours}:${minutes}`;
 }
